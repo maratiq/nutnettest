17861bf4bcd00590e9422d4031d2d3a0b7ce31e6
Zaripov Marat <zaripov2010m@gmail.com>
2022-08-14 23:26:45 +0400

    added style + css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     target: 'web',
     resolve: {
-        extensions: ['.ts', '.tsx', '.js']
+        extensions: ['.ts', '.tsx', '.js', '.css']
     },
     devServer: {
         static: {
@@ -25,7 +25,11 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
-            }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
         ],
     },
     output: {
@@ -37,4 +41,5 @@ module.exports = {
             template: path.join(__dirname, 'src', 'index.html')
         })
     ]
+
 };
