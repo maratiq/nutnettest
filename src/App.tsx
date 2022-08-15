e49b829e5ce2d505fd7c6fc31b248412aa2f6044
Zaripov Marat <zaripov2010m@gmail.com>
2022-08-15 20:00:41 +0400

    added searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import Header from "./components/Header/Header";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 const App = () => {
-    return <Header/>
+    return (
+        <Fragment>
+            <Header/>
+            <SearchBar/>
+        </Fragment>
+    )
 }
 
 export default App;
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import styles from './SearchBar.module.css';
+
+
+import Arrow from '../../assets/arrow.svg';
+
+const SearchBar = () => {
+    const [inputLocation, setInputLocation] = useState('');
+
+    const onCityClick = (e: any) => {
+        setInputLocation(e.target.textContent);
+    }
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputLocation + '&appid=6852b987d7fb620280f800f5ddfbe188&lang=ru')
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
+    }
+
+    return (
+        <form className={styles.searchBar} onSubmit={handleSubmit}>
+            <input type={"text"}
+                   className={styles.searchBar__input}
+                   placeholder={'Укажите город'}
+                   id={'searchBar'}
+                   onChange={(e: any) => setInputLocation(e.target.value)}
+                   value={inputLocation}
+            />
+            <label className={styles.searchBar__label} htmlFor={'searchBar'}>
+                <img className={styles.searchBar__arrow} src={Arrow} alt={'Стрелка'}/>
+                <div className={styles.searchBar__labelText}>
+                    <span>
+                        Начните вводить город, например, &nbsp;
+                    </span>
+                    <span className={styles.searchBar__clickableCity} onClick={onCityClick}>
+                        Ижевск
+                    </span>
+                </div>
+
+            </label>
+        </form>
+    )
+}
+
+export default SearchBar;
